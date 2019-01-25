d5673e07f89793a6522ed64044aa6d96ced36774
Eveyen <eveyenbcit@gmail.com>
2019-01-25 11:51:38 -0800

    new backup

--- mytbookbacktup.sql
+++ mytbookbacktup.sql
@@ -57,6 +57,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_email` varchar(100) DEFAULT NULL,
   `user_pass` varchar(100) DEFAULT NULL,
+  `student_id` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -79,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-23 10:52:38
+-- Dump completed on 2019-01-25 11:50:26
