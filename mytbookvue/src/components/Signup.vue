445d4446105dd56b87e6d05899a20513213223fd
Amyulim <apppe77@gmail.com>
2019-01-25 16:39:12 -0800

    .

--- mytbookvue/src/components/Signup.vue
+++ mytbookvue/src/components/Signup.vue
@@ -6,17 +6,17 @@
         <h1 class="h3 mb-3 font-weight-normal center">Please sign up </h1>
 
         <label for="inputEmail" class="sr-only">Email address</label>      
-        <input type="email" id="email" name="user_email" class="form-control" placeholder="Email address" required autofocus>
+        <input type="email" name="user_email" class="form-control" placeholder="Email address" required autofocus>
 
         <label for="inputSnum" class="sr-only">Student Number</label>
-        <input type="text" id="studentN" name="student_id" class="form-control" placeholder="A0000000" required autofocus>
+        <input type="text"  name="student_id" class="form-control" placeholder="A0000000" required autofocus>
 
         <label for="inputPassword" class="label-only">Password</label>
-        <input type="password" id="inputPassword" name="user_pass" class="form-control" placeholder="Password" required>
+        <input type="password"  name="user_pass" class="form-control" placeholder="Password" required>
 
         <label for="inputPassword" class="label-only">Password</label>
 
-        <input type="password" id="inputPassword"  class="form-control" placeholder="Re-Type Password" required>
+        <input type="password"  class="form-control" placeholder="Re-Type Password" required>
 
         <button class="btn btn-lg btn-primary btn-block submit" type="submit">Sign Up</button>
             
@@ -30,3 +30,16 @@
   @import "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css";
   @import "../components/style.css";
 </style>
+
+<script>
+  // UI Method
+
+var fd = new FormData();
+fd.append("email", this.email);
+var resp = await fetch("link_to_heroku/create.php", {
+	method:"POST",
+	body:fd
+});
+
+var json = await resp.json();
+</script>
---
+++ mytbookvue/src/components/insert_user.php
@@ -0,0 +1,30 @@
+<?php
+
+
+try {
+  $conn = new PDO("mysql://a9uyw0nx8yll3o97:karbogix1o6dosfv@k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=od7e1lmerptv21bg", "a9uyw0nx8yll3o97", "karbogix1o6dosfv");
+} catch (PDOException $e) {
+  echo "Error".$e->getMessage();
+}
+
+
+$user_email = $_POST['user_email'];
+$student_id = $_POST['student_id'];
+$user_pass = $_POST['user_pass'];
+
+
+$query = "INSERT INTO users (user_email, student_id, user_pass) VALUES ('$user_email','$student_id','$user_pass')";
+
+
+$result = $conn->query($query);
+if($result){
+  $id = $conn->lastInsertId();
+  echo json_encode(array(
+    "status"=>true,
+    "id"=>$id
+  ));
+} else {
+  echo json_encode(false);
+}
+
+?>
