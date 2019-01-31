a7c677a84720c8ddbdff79b5d2c9a92401f4b527
amyulim <apppe77@gmail.com>
2019-01-31 00:46:12 -0800

    mapping
    mapping array success

--- src/components/Booklist.vue
+++ src/components/Booklist.vue
@@ -19,19 +19,20 @@
           <div class="container">
 
             <div class="row lists">
-               <div class="col-md-4">
+               <div class="col-md-4"  v-for="item in result">
                 <div class="card mb-4 shadow-sm book-des">
                   <div class="book-img text-center">
                     <img src="../assets/book1.jpg" class="card-img-top" text="Thumbnail"/>
                   </div>
                   <div class="book-title text-center">
-                    {{result[0].book_title}}<br/><span class="price">${{result[0].book_price}}</span> 
+                    {{item.book_title}}<br/>
+                    <span class="price">${{item.book_price}}</span> 
                   </div>
     
                   <div class="card-body">               
                       <ul>
                         <li><span class="list-title">ISBN</span> : {{result[0].book_isbn}} </li>
-                        <li><span class="list-title">Course</span> : {{result[0].book_course}}</li>
+                        <li><span class="list-title">Course</span> : {{item.book_course}}</li>
                         <li><span class="list-title">Meeting Time</span> : Tue, 13, Feb</li>
                       </ul>
                
@@ -43,55 +44,8 @@
                   </div>
                 </div>
               </div>
-              <div class="col-md-4">
-                <div class="card mb-4 shadow-sm book-des">
-                  <div class="book-img text-center">
-                    <img src="../assets/book1.jpg" class="card-img-top" text="Thumbnail"/>
-                  </div>
-                  <div class="book-title text-center">
-                    Business Communication <br/><span class="price">$10</span> 
-                  </div>
-    
-                  <div class="card-body">               
-                      <ul>
-                        <li><span class="list-title">ISBN</span> : 3435939 </li>
-                        <li><span class="list-title">Course</span> : Business Com 1 </li>
-                        <li><span class="list-title">Meeting Time</span> : Tue, 13, Feb</li>
-                      </ul>
-               
-                    <div class="d-flex justify-content-between align-items-center">                
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>  
-                        <button type="button" class="btn btn-sm deal-status pull-right">In processs</button>                 
-                    </div>
-                    
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="card mb-4 shadow-sm book-des">
-                  <div class="book-img text-center">
-                    <img src="../assets/book1.jpg" class="card-img-top" text="Thumbnail"/>
-                  </div>
-                  <div class="book-title text-center">
-                    Business Communication <br/><span class="price">$10</span> 
-                  </div>
-    
-                  <div class="card-body">               
-                      <ul>
-                        <li><span class="list-title">ISBN</span> : 3435939 </li>
-                        <li><span class="list-title">Course</span> : Business Com 1 </li>
-                        <li><span class="list-title">Meeting Time</span> : Tue, 13, Feb</li>
-                      </ul>
-               
-                    <div class="d-flex justify-content-between align-items-center">                
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>  
-                        <button type="button" class="btn btn-sm deal-status pull-right">In processs</button>                 
-                    </div>
-                    
-                  </div>
-                </div>
-                
-              </div>
+            
+
             </div>
           </div>
         </div>
