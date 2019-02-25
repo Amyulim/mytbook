954de9132e946d3e4d7ba7edb306b8c72237918e
Eveyen <eveyenbcit@gmail.com>
2019-02-24 22:44:33 -0800

    server side

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8800;
+
+const server = app.listen(port,(err)=>{
+	if (err){
+		console.log(err);
+		return false;
+	}
+	console.log(port + " is opened");
+});
+
+var io = require("socket.io")(server);
+
+io.on("connection", (socket)=>{
+	console.log(socket.id + " connected");
+	
+});
