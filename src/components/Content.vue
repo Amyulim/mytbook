ae0d0835604fbc12341737ba4a72e8db01f5e846
Eveyen <eveyenbcit@gmail.com>
2019-03-03 22:10:51 -0800

    content

---
+++ src/components/Content.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="homeContent d-flex w-100 h-100 body">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-12 content-title">
+          <h4> Why should you sell and buy books using Mytbook?</h4>
+        </div>
+
+
+        <div class="row">
+
+
+          <div class="col-md-4 content-box shadow rounded">
+            <p> <span class="content-sub"> Accessibility</span> <br> Looking for learning resources like books can be expensive for some students. It will be also such a waste if we just leave those used books untouched after using them for just a semester or two. Put some good use on those books by selling them to our website.<br> Mytbook strives to be your one-stop shop for searching books you needed for school. Search for the book you needed and set up a meet up with the book owner whether inside or outside the campus.
+            </p>
+          </div>
+
+          <div class="col-md-4 content-box shadow rounded">
+            <p> <span class="content-sub"> Good Price, Good Condition</span> <br> Don't just settle for expensive books because you can't find a reliable source of used books. In Mytbook, you will not only get good quality books, you can also get them on an affordable and negotiable price. Good education doesnt come from expensive books.
+            </p>
+
+
+          </div>
+          <div class="col-md-4 content-box shadow rounded">
+            <p> <span class="content-sub"> Create a community</span> <br> Mytbook encourages BCIT students to be connected to 
+            </p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+    </div>
+
+
+  </div>
+
+</template>
