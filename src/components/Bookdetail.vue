179252ed6992e15498f1d92a468f3d9553988f95
amyulim <apppe77@gmail.com>
2019-03-04 17:42:31 -0800

    styling
    book detail - buttons

--- src/components/Bookdetail.vue
+++ src/components/Bookdetail.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="bookview-body row2">
-      <i  v-if="curItem.user_id===this.store.user_id" @click="GoBookUpdate" class="edit-button pull-right fa fa-edit fa-2x"></i>
+  <div class="row2">
+    <i v-if="curItem.user_id===this.store.user_id" @click="GoBookUpdate" class="edit-button pull-right fa fa-edit fa-2x"></i>
+    <button @click="GoMessage" type="button" class="btn-primary btn-sm msg-btn"><i class="fa fa-envelope fa-lg msg-icon"></i>Send</button>
     <div class="col-md-4 bookview-img">
       <div class="detail-img-box">
+        <img :src="img+curItem.book_id+'.jpg'" class="detail-img mt-3" text="Thumbnail" />
         <div v-if="curItem.book_status === 'null'"></div>
         <div v-else-if="curItem.book_status === null"></div>
         <div v-else-if="curItem.book_status === 'None'"></div>
-        <div v-else class="justify-content-between align-items-center">
-          {{curItem.book_status}}
-        </div>
-        <img :src="img+curItem.book_id+'.jpg'" class="detail-img" text="Thumbnail" />
+
+        <button v-else class="btn btn-sm progress-btn pull-left deal-status"> {{curItem.book_status}}</button>
       </div>
+
+
 
     </div>
 
@@ -29,10 +31,6 @@
       <hr>
       <p><span class="bookview-title">Meeting Time: </span>{{curItem.book_mdate}}</p>
       <hr>
-      <!--to message the book owner TEMPORARY-->
-
-      <button @click="GoMessage" type="button" class="btn btn-sm btn-block btn-primary send-msg-button mb-3"><i class="fa fa-envelope fa-lg"></i></button>
-
     </div>
     <div v-if="messenger == true" class="modal-bg z-index-1000">
       <div class="msg-box" id="messenger">
