08207ed2c6523a63de36bd2b7f32522ed483e181
Eveyen <eveyenbcit@gmail.com>
2019-03-05 08:42:18 -0800

    more content on homepage

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -41,18 +41,18 @@
         <div class="row">
           <div class="col-md-4 content-box shadow rounded">
             <img class="content-logo" src="../assets/accessibility.svg" alt="Accessibility logo" />
-            <p> <span class="content-sub"> Accessibility</span> <br> Looking for learning resources like books can be expensive for some students. It will be also such a waste if we just leave those used books untouched after using them for just a semester or two. Put some good use on those books by selling them to our website. <br> <br>Mytbook strives to be your one-stop shop for searching for books you needed for school. Search for the book you needed and set up a meet up with the book owner whether inside or outside the campus.
+            <p> <span class="content-sub"> Accessibility</span> <br> Looking for learning resources like books can be expensive for some students. It will be also such a waste if we just leave those used books untouched after using them for just a semester or two. Put some good use on those books by selling them to our website. <br> <br>Mytbook strives to be your one-stop shop for searching for books you needed for school. Search for the book you needed and set up a prefered meet up that suits your best availability.
             </p>
           </div>
           <div class="col-md-4 content-box shadow rounded">
 
             <img class="content-logo" src="../assets/Money.svg" alt="Money logo" />
-            <p> <span class="content-sub"> Good Price, Good Condition</span> <br> Don't just settle for expensive books because you can't find a reliable source of cheap but still in a good condition book. In Mytbook, you will not only get good quality books, but you can also get them on an affordable and negotiable price. Mytbook believes that  <em>Good education doesnt come from expensive books </em>.
+            <p> <span class="content-sub"> Good Price, Good Condition</span> <br>  Don't just settle for expensive books because you can't find a reliable source of cheap but still in a good condition book. In Mytbook, you will not only get good quality books, but you can also get them on an affordable and negotiable price whenever and wherever.<br><br> Mytbook offers a wide variety of books available for students made by other BCIT students themselves!. Mytbook believes that  <em>Good education doesnt come from expensive books </em>.
             </p>
           </div>
           <div class="col-md-4 content-box shadow rounded">
             <img class="content-logo" src="../assets/community.svg" alt="community logo" />
-            <p> <span class="content-sub"> Create a community</span> <br> Mytbook encourages BCIT students to be connected to
+            <p> <span class="content-sub"> Create a community</span> <br> Mytbook promotes the idea of creating a community. With one simple search, you’ll get connected to your fellow BCIT students who sell the book you are looking for an affordable book that is obviously a lot cheaper than their original price. In Mytbook we made it easier for customers to:<br><br>1. Directly send a message to the book owner through the app.<br> 2. meet up with the book owner based on their posted preferred meeting date whether inside or outside the campus.
             </p>
           </div>
         </div>
