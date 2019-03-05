fb4babb8da82b549d706eaf15730d0449b3f83c0
Eveyen <eveyenbcit@gmail.com>
2019-03-04 19:36:10 -0800

    homepage

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -16,13 +16,13 @@
       <div class="row">
         <div class="col-md-7 landing justify-conent-center">
           <div class="justify-content-center m-auto">
-            
+
 
             <div class="pr-5">
               <h1><span>Buy and Sell</span><br> Your Textbooks</h1>
               <p>Buy and Sell your textbooks for the best price. See every offer, and pick the best price.
               </p>
-              <router-link to="/booklist" class="link" > Browse books <i class="right arrow"></i></router-link> &emsp;
+              <router-link to="/booklist" class="link"> Browse books <i class="right arrow"></i></router-link> &emsp;
             </div>
           </div>
         </div>
@@ -32,6 +32,31 @@
         </div>
       </div>
 
+
+      <div class="container homeContent">
+        <div class="col-md-12 content-title">
+          <h3> Why should you sell and buy books using Mytbook?</h3>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4 content-box shadow rounded">
+            <img class="content-logo" src="../assets/accessibility.svg" alt="Accessibility logo" />
+            <p> <span class="content-sub"> Accessibility</span> <br> Looking for learning resources like books can be expensive for some students. It will be also such a waste if we just leave those used books untouched after using them for just a semester or two. Put some good use on those books by selling them to our website. <br> <br>Mytbook strives to be your one-stop shop for searching for books you needed for school. Search for the book you needed and set up a meet up with the book owner whether inside or outside the campus.
+            </p>
+          </div>
+          <div class="col-md-4 content-box shadow rounded">
+
+            <img class="content-logo" src="../assets/Money.svg" alt="Money logo" />
+            <p> <span class="content-sub"> Good Price, Good Condition</span> <br> Don't just settle for expensive books because you can't find a reliable source of cheap but still in a good condition book. In Mytbook, you will not only get good quality books, but you can also get them on an affordable and negotiable price. Mytbook believes that  <em>Good education doesnt come from expensive books </em>.
+            </p>
+          </div>
+          <div class="col-md-4 content-box shadow rounded">
+            <img class="content-logo" src="../assets/community.svg" alt="community logo" />
+            <p> <span class="content-sub"> Create a community</span> <br> Mytbook encourages BCIT students to be connected to
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
 
 
