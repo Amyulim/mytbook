5a4d2ffaf2374ded1419223a20f7681291bf3545
Eveyen <eveyenbcit@gmail.com>
2019-03-16 14:36:52 -0700

    condition

--- src/components/Bookform.vue
+++ src/components/Bookform.vue
@@ -166,14 +166,15 @@
       Bookform: async function() {
        
         console.log(this.book_condition)
+        
         if ( this.book_condition == "Vey Good"){
-           this.book_condition = 1
+           this.book_condition = "1"
         } else if( this.book_condition == "Good"){
-           this.book_condition = 2
+           this.book_condition = "2"
         } else if ( this.book_condition == "Fair"){
-           this.book_condition = 3
+           this.book_condition = "3"
         } else {
-           this.book_condition = 4
+           this.book_condition = "4"
         }
         
         var fd = new FormData();
